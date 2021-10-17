8554ee97d06401dbb1dac2afc687e7efb2e0ef16
Niteesha-Balne <niteeshabalne@gmail.com>
2021-10-17 12:44:29 +0530

    changes

--- client.py
+++ client.py
@@ -35,21 +35,21 @@ def main():
             client.send(f"{cmd}@{data[1]}".encode(FORMAT))
         elif cmd == "UPLOAD":
             path = data[1]
-            lines_per_file = 300
-			smallfile = None
             with open(f"{path}", "r") as f:
-                bigfile = f.read()
-    		for lineno, line in enumerate(bigfile):
-        		if lineno % lines_per_file == 0:
+                bigfile = f.read()       
+            smallfile = None
+            lines_per_file = 300
+            for lineno, line in enumerate(bigfile):
+            	if lineno % lines_per_file == 0:
             		if smallfile:
-                		smallfile.close()
+            			smallfile.close()
             		small_filename = 'small_file_{}.txt'.format(lineno + lines_per_file)
             		smallfile = open(small_filename, "w")
-        		smallfile.write(line)
-        		with open(small_filename, "r") as f:
-                	text = f.read()
-                sfname = path.split("/")[-1]	
-        		send_data = f"{cmd}@{sfname}@{text}"
+            	smallfile.write(line)
+            	with open(small_filename, "r") as f:
+            		text = f.read()
+            	sfname = path.split("/")[-1]
+            	send_data = f"{cmd}@{sfname}@{text}"
             	client.send(send_data.encode(FORMAT))
 
     print("Disconnected from the server.")
@@ -57,3 +57,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+         	
+
+
+
+    				
+    			
+    			
+    			
+    			
